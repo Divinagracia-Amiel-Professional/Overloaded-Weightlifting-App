4c06254d341947c30275d8c25af9f40b4d487100
Amiel Osias Divinagracia <amielosiasddivinagracia@iskolarngbayan.pup.edu.ph>
2023-10-10 23:58:15 +0800

    updating theme before importing google fonts

---
+++ constants/theme.js
@@ -0,0 +1,51 @@
+const colors = {
+    primary: "#880D1E", //OU Crimson
+    secondary: "#3D2B3D", //Dark Purple
+  
+    black: "4F5152", //Cracked Pepper
+
+    lightGray: "#F0EEEA", //Isabelline
+    lightGray2: "#DAD7CD", //TimberWolf
+  
+    white: "#FFF",
+  };
+  
+  const font = {
+    regular: "DMRegular",
+    medium: "DMMedium",
+    bold: "DMBold",
+  };
+  
+  const sizes = {
+    xSmall: 10,
+    small: 12,
+    medium: 16,
+    large: 20,
+    xLarge: 24,
+    xxLarge: 32,
+  };
+  
+  const shadow = {
+    small: {
+      shadowColor: "#000",
+      shadowOffset: {
+        width: 0,
+        height: 2,
+      },
+      shadowOpacity: 0.25,
+      shadowRadius: 3.84,
+      elevation: 2,
+    },
+    medium: {
+      shadowColor: "#000",
+      shadowOffset: {
+        width: 0,
+        height: 2,
+      },
+      shadowOpacity: 0.25,
+      shadowRadius: 5.84,
+      elevation: 5,
+    },
+  };
+  
+  export { colors, font, sizes, shadow };
