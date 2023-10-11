af79184524ff3b4790dd52bf2508bba913d3a9d4
Amiel Osias Divinagracia <amielosiasddivinagracia@iskolarngbayan.pup.edu.ph>
2023-10-11 22:45:45 +0800

    - changed text style in home

--- pages/home.js
+++ pages/home.js
@@ -10,7 +10,7 @@ import {
 export default function Home() {
   return (
     <View style={mainStyles.bodyContainer}>
-      <Text style={mainStyles.headerText}>Description: </Text>
+      <Text style={textStyles.headerText}>Description: </Text>
       <Text style={textStyles.bodyText}>Venenatis maecenas at ullamcorper risus iaculis at imperdiet nulla. Amet sed est porttitor ridiculus elementum. Non sem leo nunc quisque. Nec lobortis nisl blandit viverra amet tortor lorem elit mattis. Integer hendrerit egestas blandit at blandit ut. Arcu faucibus vitae in leo gravida auctor. Purus erat lacus enim viverra curabitur. Malesuada tempor neque in sed habitasse mi. Magna tristique consectetur commodo duis cursus enim massa. Mi varius posuere amet est. Tristique nisl ipsum massa eu dui sed feugiat et.</Text>
       <Pressable style={buttonStyles.button}>
         <Text style={buttonStyles.buttonText}>Add to workouts</Text>
