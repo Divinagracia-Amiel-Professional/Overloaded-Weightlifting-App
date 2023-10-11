2e27d19649f45f668f35a4a0c1e5b5378fede21b
Amiel Osias Divinagracia <amielosiasddivinagracia@iskolarngbayan.pup.edu.ph>
2023-10-11 23:46:36 +0800

    - added navbar, header, start-button placeholders - added return function in planner, settings, tracker pages - removed button import in button-styles - added iconText style in text styles - changed logonavicon to logoIcon in icons.js

--- constants/icons.js
+++ constants/icons.js
@@ -1,11 +1,11 @@
 import calendarIcon from '../assets/icons/calendar-navbar-default'
 import gearIcon from '../assets/icons/gear-navbar-default'
 import graphIcon from '../assets/icons/graphIcon-navbar-default'
-import logoNavIcon from '../assets/icons/logo-navbar-default'
+import logoIcon from '../assets/icons/logo-navbar-default'
 
 export {
     calendarIcon,
     gearIcon,
     graphIcon,
-    logoNavIcon
+    logoIcon
 }
