85a12a5f41e492201a437ef435f6340ab870fbc3
Amiel Osias Divinagracia <amielosiasddivinagracia@iskolarngbayan.pup.edu.ph>
2023-12-05 23:09:54 +0800

    -implemented persistence using expo-file-system and redux persist

--- redux/store.ts
+++ redux/store.ts
@@ -1,4 +1,7 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
+import storage from 'redux-persist/lib/storage';
+import { persistReducer, persistStore } from 'redux-persist';
+
 import exerciseReducer from './slices/ExerciseSlice'
 import workoutReducer from "./slices/WorkoutSlice";
 import initReducer from "./slices/InitSlice";
@@ -6,16 +9,28 @@ import currentUserReducer from "./slices/CurrentUserSlice";
 import themeReducer from "./slices/ThemeSlice";
 import currentSplitReducer from "./slices/CurrentSplit";
 
+import ExpoFileSystemStorage from "redux-persist-expo-filesystem";
+
+const rootReducer = combineReducers({
+    exercise: exerciseReducer,
+    workout: workoutReducer,
+    init: initReducer,
+    currentUser: currentUserReducer,
+    currentSplit: currentSplitReducer,
+    theme: themeReducer,
+})
+
+const persistConfig = {
+  key: "root",
+  storage: ExpoFileSystemStorage,
+};
+
+const persistedReducer =  persistReducer(persistConfig, rootReducer)
+
 export const store = configureStore({
-    reducer: {
-        exercise: exerciseReducer,
-        workout: workoutReducer,
-        init: initReducer,
-        currentUser: currentUserReducer,
-        currentSplit: currentSplitReducer,
-        theme: themeReducer,
-    }
+    reducer: persistedReducer
 });
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
+export const persistor = persistStore(store)
