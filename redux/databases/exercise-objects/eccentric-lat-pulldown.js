d483bd553c2e2d9e0be7fd552622b23f7c22e82a
Jeremy R. Mendez <iamjeremymendez234@gmail.com>
2024-01-13 01:12:38 +0800

    last exercises

---
+++ redux/databases/exercise-objects/eccentric-lat-pulldown.js
@@ -0,0 +1,35 @@
+export default 
+{
+    id: "ZdZ4lagRZDvDB6TXokMt",
+    name: "Eccentric Lat Pulldown",
+    description: "The lat pulldown, like the bent-over row, is one of the most popular exercises used to strengthen the muscles of the back. ",
+    difficulty: 2,
+    focus: [
+        "delts_front",
+        "delts_mid",
+        "delts_back", 
+        "tri_long",
+        "tri_lat",
+        "tri_mid"
+    ],
+    group: [
+        "arms",
+        "pull",
+    ],
+    resources: {
+        img_urls: [
+            {
+                url: "https://i.ytimg.com/vi/cxqyLsDjRJw/maxresdefault.jpg",
+                title: "Proper Execution of Eccentric Accentuated Lat Pulldown - Up with 2: Down with 1 Technique",
+                description: "Want to maximize growth in your upper back and lats?  Then take advantage of eccentric training.  Eccentric overload has consistently been shown to be one of the most effective training protocols for improving functional strength and hypertrophy as well as helping with injury prevention."
+            }
+        ],
+        vid_urls: [
+            {
+                url: "https://www.youtube.com/watch?v=v9CaregmK3s#:~:text=The%20underhand%20grip%20lat%20pulldown,optimal%2090%20degree%20joint%20angles.",
+                title: "Eccentric Isometric Underhand Lat Pulldowns with Proper Form",
+                description: "The underhand grip lat pulldown is a great vertical pulling exercise for crushing the lats while also blasting the biceps as it places the arms into a biomechanically stronger position to handle greater loads."
+            }
+        ]
+    }
+}
---
+++ redux/databases/exercise-objects/reverse-grip-pullup.js
@@ -0,0 +1,35 @@
+export default 
+{
+    id: "EZubBDYneHHINa819MI2",
+    name: "Reverse Grip Pull Up",
+    description: "Pull-ups are prime among back exercises, however, they are not a particularly easy exercise, and as such many people may not be able to perform them.",
+    difficulty: 4,
+    focus: [
+        "bicep_short",
+        "bicep_long",
+        "lats", 
+        "teres_major",
+        "traps_upper"
+    ],
+    group: [
+        "arms",
+        "back",
+        "calisthenics"
+    ],
+    resources: {
+        img_urls: [
+            {
+                url: "https://staticg.sportskeeda.com/editor/2023/06/2286d-16869394136261-1920.jpg",
+                title: "How to Do Reverse Grip Pull-Ups: Benefits, Tips, Techniques and Variations",
+                description: "Pull-ups are considered the king of upper body workouts and are best for developing strength. Reverse grip pull-ups offer much of the same benefits."
+            }
+        ],
+        vid_urls: [
+            {
+                url: "https://www.youtube.com/watch?v=LIMydLgfAeY",
+                title: "Exercise Videos- Reverse-Grip Pull-up",
+                description: "The reverse grip pull up is a great exercise that strengthens and tones your back.  It also strengthens your biceps and grip at the same time."
+            }
+        ]
+    }
+}
