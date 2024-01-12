5bf706a8b9482b5736ef93eabdf186de24b30d3a
Jeremy R. Mendez <iamjeremymendez234@gmail.com>
2024-01-12 18:20:59 +0800

    edit other exercises

--- redux/databases/exercise-objects/deadlift-conventional.js
+++ redux/databases/exercise-objects/deadlift-conventional.js
@@ -1,37 +1,41 @@
 export default 
 {
-    id: "KN9JnBNyeMitbfEoumYd",
+    id: "cLyNe2g2CC643wBaBfhO",
     name: "Conventional Deadlift",
     description: "",
-    difficulty: 4,
+    difficulty: 5,
     focus: [
         "glut_max", 
         "hams", 
-        "erector",
+        "lats",
         "quad",
         "forearm",
-        "traps",
+        "traps_upper",
+        "traps_lower",
         "core"
     ],
     group: [
         "legs",
         "core",
         "full",
-        "back"
+        "back",
+        "arms",
+        "chest"
+
     ],
     resources: {
         img_urls: [
             {
-                url: "",
-                title: "",
-                description: ""
+                url: "https://cdn.muscleandstrength.com/sites/default/files/deadlift_0.jpg",
+                title: "Conventional Deadlift Video Exercise Guide",
+                description: "The deadlift is an extremely popular exercise and a true test of total body strength. It is popular across numerous weight lifting circle including bodybuilders, powerlifts, and Crossfit athletes."
             }
         ],
         vid_urls: [
             {
-                url: "",
-                title: "",
-                description: ""
+                url: "https://www.youtube.com/watch?v=GxsLrTzyGUU",
+                title: "How to Do A Conventional Deadlift Correctly",
+                description: "We're breaking down the classic conventional deadlift to help you improve your technique and form. Getting the correct deadlift form will help you to target the right muscles and avoid injuries."
             }
         ]
     }
--- redux/databases/exercise-objects/hip-thrust-bar.js
+++ redux/databases/exercise-objects/hip-thrust-bar.js
@@ -1,9 +1,9 @@
 export default 
 {
-    id: "3",
+    id: "IzLDSZ1FSg9X6tgTRqIn",
     name: "Hip Thrust",
-    description: "",
-    difficulty: 3,
+    description: "A barbell hip thrust is a lower-body strength training exercise defined by lifting your lower back and torso with your knees bent and your upper body resting on a bench. ",
+    difficulty: 2,
     focus: [
         "glut_max", 
         "glut_med",
@@ -17,16 +17,16 @@ export default
     resources: {
         img_urls: [
             {
-                url: "",
-                title: "",
-                description: ""
+                url: "https://images.contentful.com/3s5io6mnxfqz/20ZiF7JBA3OCly1aiaGpfO/05fb667a938fda819216586d69850ca7/MasterClassSEO_Barbell_Hip_Thrust_ONE.jpg",
+                title: "How to Do a Barbell Hip Thrust",
+                description: "With proper form, the barbell hip thrust works muscle groups across your entire lower body, particularly the gluteal muscles."
             }
         ],
         vid_urls: [
             {
-                url: "",
-                title: "",
-                description: ""
+                url: "https://www.youtube.com/watch?v=aweBS7K71l8",
+                title: "How To Do A Barbell Hip Thrust",
+                description: "Once you've mastered the bodyweight hip thrust, adding weight can help to overload the glutes and hamstrings and continue to build strength and size. The barbell is the most popular way to add weight to hip thrusts."
             }
         ]
     }
