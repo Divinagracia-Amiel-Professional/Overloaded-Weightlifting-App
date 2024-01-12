b2c131249d1a366713a77976386df18708908855
Amiel Osias Divinagracia <amielosiasddivinagracia@iskolarngbayan.pup.edu.ph>
2024-01-12 08:29:35 +0800

    -added some comments in current user slice

--- redux/slices/CurrentUserSlice.js
+++ redux/slices/CurrentUserSlice.js
@@ -14,18 +14,18 @@ const CurrentUserSlice = createSlice({
     name: "CurrentUser",
     initialState: initState,
     reducers: {
-        addWorkout: (state, action) => {
+        addWorkout: (state, action) => { //Used in Adding Workouts; CreateFromScratchPage and PremadeWorkoutDetails Page   
             state.workoutUsed = [...state.workoutUsed, action.payload]
         },
-        deleteWorkout: (state, action) => {
+        deleteWorkout: (state, action) => { //Used in Manage Workout Page if user wants to delete workout
             const workoutIndex = state.workoutUsed.findIndex(workout => workout.id === action.payload)
 
             state.workoutUsed.splice(workoutIndex, 1)
         },
-        useWorkout: (state, action) => {
+        useWorkout: (state, action) => { //Used in Manage Workout Page if user wants to use workout
             state.currentWorkout = action.payload
         },
-        updateState: (state, action) => {
+        updateState: (state, action) => { //Used in Workout Picker in Start Button Details
             if(state.workoutUsed[0]){
                 const workoutIndex = state.workoutUsed.findIndex(workout => workout.id === action.payload.workoutId)
                 const data = {
@@ -39,8 +39,8 @@ const CurrentUserSlice = createSlice({
                 console.log('There is no workout with that ID')
             }
         },
-        completeWorkout: (state, action) => {
-            if(state.workoutUsed[0]){
+        completeWorkout: (state, action) => { //PostWorkoutPage
+            if(state.workoutUsed[0]){ 
                 const workoutIndex = state.workoutUsed.findIndex(workout => workout.id === action.payload)
 
                 state.workoutUsed[workoutIndex].latest_state.is_completed = true
@@ -51,19 +51,19 @@ const CurrentUserSlice = createSlice({
             }
         },
         updateRecords: (state, action) => {
-            const recordsData = action.payload.recordsData
-            const isStateRecordsEmpty = state.records[0] ? false : true
+            const recordsData = action.payload.recordsData //data from PostWorkoutPage
+            const isStateRecordsEmpty = state.records[0] ? false : true //check if state records is empty
 
             recordsData.forEach(record => {
-                const hasExerciseRecord = !isStateRecordsEmpty ? state.records.some(stateRecord => {
+                const hasExerciseRecord = !isStateRecordsEmpty ? state.records.some(stateRecord => { //check if exercise already exists in redux
                     return stateRecord.id === record.id
                 }) : false
 
                 if(hasExerciseRecord){
-                    const exerciseIndex = state.records.findIndex(stateRecord => stateRecord.id === record.id)
+                    const exerciseIndex = state.records.findIndex(stateRecord => stateRecord.id === record.id) 
                     const thisExerciseRecord = state.records[exerciseIndex]
 
-                    state.records[exerciseIndex] = {
+                    state.records[exerciseIndex] = { //add records to existing ones
                         ...thisExerciseRecord,
                         records: [
                             ...thisExerciseRecord.records,
@@ -96,12 +96,12 @@ const CurrentUserSlice = createSlice({
 
             console.log(state.records)
         },
-        resetRecords: (state) => {
+        resetRecords: (state) => { //Delete every record
             state.records = []
 
             console.log(state.records)
         },
-        consoleLogRecords: (state) => {
+        consoleLogRecords: (state) => { //For Debugging
             console.log(JSON.stringify(state.records))
         }
     },
