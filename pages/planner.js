ebba9a8e5b1351943063fb4f59370a3303b24e36
Amiel Osias Divinagracia <amielosiasddivinagracia@iskolarngbayan.pup.edu.ph>
2024-01-16 18:28:40 +0800

    -Made planner page a scroll view

--- pages/planner.js
+++ pages/planner.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Text, View, Pressable } from 'react-native';
+import { Text, View, Pressable, ScrollView, StyleSheet } from 'react-native';
 import { useTheme } from 'react-native-paper';
 import {
     mainStyles
@@ -12,6 +12,7 @@ import { MaterialCommunityIcons } from '@expo/vector-icons';
 import { Add, CalendarEdit } from 'iconsax-react-native'
 import { Rack } from '../constants/icons';
 import { getUserWorkouts, getAllExercises } from '../functions/functions-index';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function Planner({navigation}){
     const theme = useTheme()
@@ -31,45 +32,65 @@ export default function Planner({navigation}){
     console.log(userWorkout)
 
     return(
-        <View style={{...mainStyles.bodyContainer,
-            backgroundColor: theme.colors.background
-        }}>
-            <CardButton 
-                state={true}
-                header='Create Workout'
-                mainIcon={<Rack fill={theme.colors.background}/>}
-                mainIconDisabled={<Rack fill={theme.colors.onTertiaryContainer}/>}
-                sideIcon={<Add size='32' color={theme.colors.background} />}
-                sideIconDisabled={<Add size='32' color={theme.colors.onTertiaryContainer} />}
-                disabledMessage='Cannot Create Workout'
-                onPress={() => {
-                    navigation.navigate('SelectionPage')
+        <SafeAreaView
+            style={{flex: 1, position: 'relative'}}
+        >
+            <ScrollView
+                style={{...style.container,
+                    backgroundColor: theme.colors.background
                 }}
-            />
-            <CardButton 
-                state={userWorkout[0] ? true : false}
-                header='Manage Workout'
-                mainIcon={<CalendarEdit size={80} color={theme.colors.background} />}
-                mainIconDisabled={<CalendarEdit size={80} color={theme.colors.onTertiaryContainer} />}
-                sideIcon={null}
-                sideIconDisabled={null}
-                disabledMessage='Create Workout First!'
-                onPress={() => {
-                    userWorkout[0] ? navigation.navigate('ManageWorkoutPage', {userWorkout: userWorkout}) : null
-                }} 
-            />
-            <CardButton 
-                state={exercises[0] ? true : false}
-                header='Exercise List'
-                mainIcon={<FontAwesome5 name="clipboard-list" size={80} color={theme.colors.background} />}
-                mainIconDisabled={<FontAwesome5 name="clipboard-list" size={80} color={theme.colors.onTertiaryContainer} />}
-                sideIcon={null}
-                sideIconDisabled={null}
-                disabledMessage='Unavailable for now'
-                onPress={() => {
-                    exercises[0] ? navigation.navigate('ExerciseListPage', {userWorkout: userWorkout}) : null
-                }}
-            />
-        </View>
+                contentContainerStyle={style.contentContainer}
+            >
+                <CardButton 
+                    state={true}
+                    header='Create Workout'
+                    mainIcon={<Rack fill={theme.colors.background}/>}
+                    mainIconDisabled={<Rack fill={theme.colors.onTertiaryContainer}/>}
+                    sideIcon={<Add size='32' color={theme.colors.background} />}
+                    sideIconDisabled={<Add size='32' color={theme.colors.onTertiaryContainer} />}
+                    disabledMessage='Cannot Create Workout'
+                    onPress={() => {
+                        navigation.navigate('SelectionPage')
+                    }}
+                />
+                <CardButton 
+                    state={userWorkout[0] ? true : false}
+                    header='Manage Workout'
+                    mainIcon={<CalendarEdit size={80} color={theme.colors.background} />}
+                    mainIconDisabled={<CalendarEdit size={80} color={theme.colors.onTertiaryContainer} />}
+                    sideIcon={null}
+                    sideIconDisabled={null}
+                    disabledMessage='Create Workout First!'
+                    onPress={() => {
+                        userWorkout[0] ? navigation.navigate('ManageWorkoutPage', {userWorkout: userWorkout}) : null
+                    }} 
+                />
+                <CardButton 
+                    state={exercises[0] ? true : false}
+                    header='Exercise List'
+                    mainIcon={<FontAwesome5 name="clipboard-list" size={80} color={theme.colors.background} />}
+                    mainIconDisabled={<FontAwesome5 name="clipboard-list" size={80} color={theme.colors.onTertiaryContainer} />}
+                    sideIcon={null}
+                    sideIconDisabled={null}
+                    disabledMessage='Unavailable for now'
+                    onPress={() => {
+                        exercises[0] ? navigation.navigate('ExerciseListPage', {userWorkout: userWorkout}) : null
+                    }}
+                />
+            </ScrollView>
+        </SafeAreaView>
     )
 }
+
+const style = StyleSheet.create({
+    container: {
+        flex: 1,
+        padding: 15,
+        paddingTop: 30,
+    },
+    contentContainer: {
+        alignItems: 'center',
+        justifyContent: 'flex-start',
+        gap: 10,
+    }
+})
