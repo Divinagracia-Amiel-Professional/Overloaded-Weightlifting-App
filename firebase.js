ee2175c2123658e867b3495e1252765ae3aa31e6
NBright24 <78198636+NBright24@users.noreply.github.com>
2024-01-17 18:13:20 +0800

    REDONE Firebase and Profile
    /// RESET FIREBASE AND PROFILE
    >Recreated the files, if you need additional firebase components, re add it.
    > Recreated Profile Page from scratch as well.

--- firebase.js
+++ firebase.js
@@ -1,7 +1,5 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp, getApps, getApp } from "firebase/app";
-import { getAuth } from 'firebase/auth'
-import { getFirestore } from 'firebase/firestore'
+import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -9,24 +7,15 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyCen06Ej19k2k0K4IRlnVAQbsypWD6uhWw",
-    authDomain: "overloaded-app.firebaseapp.com",
-    projectId: "overloaded-app",
-    storageBucket: "overloaded-app.appspot.com",
-    messagingSenderId: "702603637326",
-    appId: "1:702603637326:web:bc98f6ba5353b59ddf091f",
-    measurementId: "G-3NDLW719RP"
+  apiKey: "AIzaSyCen06Ej19k2k0K4IRlnVAQbsypWD6uhWw",
+  authDomain: "overloaded-app.firebaseapp.com",
+  projectId: "overloaded-app",
+  storageBucket: "overloaded-app.appspot.com",
+  messagingSenderId: "702603637326",
+  appId: "1:702603637326:web:bc98f6ba5353b59ddf091f",
+  measurementId: "G-3NDLW719RP"
 };
 
 // Initialize Firebase
-let app;
-if ( getApps().length === 0 ){
-    app = initializeApp(firebaseConfig);
-} else {
-    app = getApp();
-}
-
-const auth = getAuth(app)
-const db = getFirestore(app) 
-
-export { auth, app, db }
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
--- pages/profile.tsx
+++ pages/profile.tsx
@@ -1,136 +1,118 @@
-import React from 'react';
-import { Text, View,  Pressable, KeyboardAvoidingView, StyleSheet, TextInput, TouchableOpacity } from 'react-native';
-import {
-    mainStyles,
-    textStyles,
-    buttonStyles
-} from '../styles/style-index'
-import GetWorkoutData from '../functions/getWorkoutData';
-import useExercises from '../custom-hooks/useExercises';
-import { useSelector, useDispatch } from 'react-redux';
-import { RootState, AppDispatch } from '../redux/store';
-import exercisesInitDb from '../redux/databases/exercises-init-db';
-import workoutsInitDb from '../redux/databases/workouts-init-db';
-import { addInitExercises } from '../redux/slices/ExerciseSlice';
-import { addInitWorkouts } from '../redux/slices/WorkoutSlice';
-import { ButtonWithIcon } from '../components/component-index';
-import { db, auth } from '../firebase'
+import React from "react";
+import { KeyboardAvoidingView, StyleSheet, Text, TextInput, View } from "react-native";
+import { TouchableOpacity } from "react-native-gesture-handler";
+import { auth } from "../firebase";
 
-export default function Profile(){
-    const dispatch = useDispatch<AppDispatch>()
-    const path = ['Workouts', 'gxgmfK5Z0bvhN1FkodoQ', 'Cycles', 'cycle_two','Splits']
-    const collections = useExercises(db, path)
-    console.log(collections)
-
-    const [Email, setEmail] = React.useState('')
-    const [Password, setPassword] = React.useState('')
+const LoginScreen = () => {
+    const [email, setEmail] = React.useState("");
+    const [password, setPassword] = React.useState("");
 
     const handleSignUp = () => {
         auth
-        .createUserWithEmailAndPassword(Email, Password)
-        .then((userCredential) => {
-            const user = userCredential.user;
-            console.log(user.Email);
-        })
-        .catch((error) => {alert(error.message)})
-    }
+            .createUserWithEmailAndPassword(email, password)
+            .then(userCredentials => {
+                const user = userCredentials.user;
+                console.log("Registered with:", user.email);
+            })
+            .catch(error => alert(error.message));
+    };
 
-    return(         
-        // <View style={mainStyles.bodyContainer}>
-        //     <Text style={textStyles.headerText}>Profile</Text>
-        //     <ButtonWithIcon
-        //         style={{paddingVertical: 10, opacity: 0.8}}
-        //         text="Reset Workout DB"
-        //         onPress={() => {
-        //             dispatch(addInitWorkouts(workoutsInitDb))
-        //         }}
-        //     />
-        //     <ButtonWithIcon
-        //         style={{paddingVertical: 10, opacity: 0.8}}
-        //         text="Reset Exercise DB"
-        //         onPress={() => {
-        //             dispatch(addInitExercises(exercisesInitDb))
-        //         }}
-        //     />
+    const handleLogin = () => {
+        auth
+            .signInWithEmailAndPassword(email, password)
+            .then(userCredentials => {
+                const user = userCredentials.user;
+                console.log("Logged in with:", user.email);
+            })
+            .catch(error => alert(error.message));
+    };
 
-        // </View>
-        <KeyboardAvoidingView 
-            style={styles.container}
-            behavior="padding"
-            >
-                <View style={styles.inputContainer}>
-                    <TextInput style={styles.input}
-                    value={Email}
-                    onChangeText={(text) => setEmail(text)}
-                    placeholder="Email"/>
-                    <TextInput style={styles.input} 
-                    value={Password}
-                    onChangeText={(text) => setPassword(text)}
-                    placeholder="Password" secureTextEntry/>
+    return (
+        <KeyboardAvoidingView
+        style={styles.container}
+        behavior="padding"
+        >
+            <View style={styles.inputContainer}>
+                <TextInput
+                placeholder="Email"
+                value={email}
+                onChangeText={text => setEmail(text)}
+                style={styles.input}
+                />
+                <TextInput
+                placeholder="Password"
+                value={password}
+                onChangeText={text => setPassword(text)}
+                style={styles.input}
+                secureTextEntry
+                />
+            </View>
 
-                </View>
-                <View style={styles.buttonContainer}>
-                    <TouchableOpacity onPress={() => {}} style={styles.button}>
-                        <Text style={styles.buttonText}>Login</Text>
-                    </TouchableOpacity>
-                    <TouchableOpacity onPress={handleSignUp} style={[styles.button, styles.buttonOutline]}>
-                        <Text style={styles.buttonOutlineText}>Register</Text>
-                    </TouchableOpacity>
-                </View>
-            </KeyboardAvoidingView>
+            <View style={styles.buttonContainer}>
+                <TouchableOpacity
+                onPress={handleLogin}
+                style={styles.button}
+                >
+                    <Text style={styles.buttonText}>Login</Text>
+                </TouchableOpacity>
+
+                <TouchableOpacity
+                onPress={handleSignUp}
+                style={[styles.button, styles.buttonOutline]}
+                >
+                    <Text style={styles.buttonOutlineText}>Register</Text>
+                </TouchableOpacity>
+            </View>
+
+        </KeyboardAvoidingView>
     )
 }
 
+export default LoginScreen
+
 const styles = StyleSheet.create({
-    container: {
+    container:{
         flex: 1,
-        backgroundColor: 'white',
-        justifyContent: 'center'
+        justifyContent: "center",
+        alignItems: "center",
     },
     inputContainer: {
-        width: '80%',
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
+        width: "80%",
+    },     
     input: {
-        backgroundColor: 'white',
-        padding: 15,
+        backgroundColor: "white",
+        paddingHorizontal: 15,
         paddingVertical: 10,
         borderRadius: 10,
-        marginTop: 10,
-        },
+        marginTop: 5,
+    },
     buttonContainer: {
-        width: '60%',
-        flexDirection: 'row',
-        justifyContent: 'center',
-        alignItems: 'center',
+        width: "60%",
+        justifyContent: "center",
+        alignItems: "center",
         marginTop: 40,
-        flex: 1,
     },
     button: {
-        backgroundColor: 'blue',
+        backgroundColor: "blue",
+        width: "100%",
         padding: 15,
         borderRadius: 10,
-        width: '100%',
-        alignItems: 'center',
-        color: 'white'
+        alignItems: "center",
     },
     buttonText: {
-        color: '#fff',
+        color: "white",
+        fontWeight: "700",
         fontSize: 16,
-        borderWidth: 1,
-        borderColor: 'blue'
-    },
-    buttonOutlineText: {
-        color: 'blue',
-        fontSize: 16,
-        fontWeight: 'bold'
     },
     buttonOutline: {
-        backgroundColor: 'white',
-        borderWidth: 1,
-        borderColor: 'blue',
-        marginLeft: 10
+        backgroundColor: "white",
+        marginTop: 5,
+        bprdercolor: "blue",
+        borderWidth: 2
+    },
+    buttonOutlineText: {
+        color: "blue",
+        fontWeight: "700",
+        fontSize: 16,
     }
 })
